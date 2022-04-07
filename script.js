625311a453d45a62900bc566be131e1ce3c8d44d
Michał <94971040+Michalica1@users.noreply.github.com>
2022-04-07 12:56:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//losowanie liczby
+var wylosowanaLiczba=Math.floor(Math.random()*100)+1;
+//wyswietlanie liczby
+function wyborLiczby(){
+    var ilosc=document.getElementById("zakres").value;
+    document.getElementById("wybranaLiczba").value=ilosc;
+    wylosowanaLiczba=Math.floor(Math.random()*ilosc)+1;
+    iloscProb=0;
+}
+//liczba prób
+var iloscProb=0
+function sprawdzLiczbe(){
+    iloscProb++;
+    dane=document.getElementById('liczba')
+    if(dane.value==wylosowanaLiczba){
+        alert("Wygrałeś. Ilość prób to: "+iloscProb)
+    }else if(dane.value<wylosowanaLiczba){
+        alert("Za mała liczba")
+    }else{
+        alert("Za duża liczba")
+    }
+}
